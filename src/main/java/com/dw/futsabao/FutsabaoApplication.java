8586f0eb3e242fb36253af20abb647a806c0e8a3
T0madon <joao.daciuk@gmail.com>
2025-05-08 21:53:49 -0300

    criando projeto trab 2

---
+++ src/main/java/com/dw/futsabao/FutsabaoApplication.java
@@ -0,0 +1,13 @@
+package com.dw.futsabao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutsabaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutsabaoApplication.class, args);
+	}
+
+}
